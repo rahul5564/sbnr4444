dbcdacd708868b8f36a9c5f99b50736b3fb4f4db
rahul <sraghavendra5564@gmail.com>
2021-01-19 14:04:39 +0530

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function createheart() {
+    const heart = document.createElement("div")
+
+    heart.classList.add("heart");
+
+    heart.innerText = "💜";
+
+    heart.style.left = Math.random() * 100 + "vw"; 
+
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    document.body.appendChild(heart);
+}
+
+setInterval(createheart,300);
